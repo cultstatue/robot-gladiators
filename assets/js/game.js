ad3acbc4d9ca3b59cc6f7f90ac9c88cb0e0e69cd
cultstatue <saltedchurros@gmail.com>
2022-05-31 13:42:19 -0400

    addes basic javascript function and variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert! JavaScript is running!");
+var playername = window.prompt("What is your robot's name?")
+console.log(playername);
+
+console.log("This log is a String, good for leavbing yourself a message");
+
+console.log(10+10);
+
+console.log("Our robots name is " + playername);
+function fight() {
+
+    window.alert("The fight has begun!");
+
+}
+
+// fight();
